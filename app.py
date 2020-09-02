54b1f25933102bfb2218ea6fdba5337156a6c175
DESKTOP-3G06E02\Antoine <antoinekeribin@hotmail.fr>
2020-09-02 17:53:08 +0200

    commit final : liste de véhicules créer et afficher + gestion droits users

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 username = ""
 
+vehiculeList = []
+
 @app.route("/")
 def index():
     return render_template('index.html', username = username)
@@ -19,7 +21,8 @@ def userlogin():
         print(username)
         redirect(request.url)
         if username == "client":
-            return render_template('vehiculeslist.html', username = username)
+            vehiculeList = fillVehiculeList()
+            return render_template('vehiculeslist.html', username = username, vehiculeList = vehiculeList)
         if username == "vendeur":
             return render_template('dashboard.html', username = username)
         if (username != 'vendeur' and username != "client"):
@@ -28,7 +31,9 @@ def userlogin():
 
 @app.route("/vehicules")
 def vehicules():
-    return render_template('vehiculeslist.html', username = username)
+    vehiculeList = fillVehiculeList()
+    print(vehiculeList)
+    return render_template('vehiculeslist.html', username = username, vehiculeList = vehiculeList)
 
 @app.route("/visiteVirtuelle")
 def visiteVirtuelle():
@@ -37,6 +42,26 @@ def visiteVirtuelle():
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html', username = username)
+
+def fillVehiculeList():
+    data = []
+    maker = "BMW"
+    model = "série 1"
+    mileage = 130000
+    data.append((maker, model, mileage))
+    maker = "BMW"
+    model = "X5"
+    mileage = 145028
+    data.append((maker, model, mileage))
+    maker = "Volkswagen"
+    model = "Golf"
+    mileage = 10000
+    data.append((maker, model, mileage))
+    maker = "Nissan"
+    model = "Juke"
+    mileage = 237644
+    data.append((maker, model, mileage))
+    return data
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
